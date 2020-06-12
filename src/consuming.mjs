fa005fe1a112d391802ee282de5b062147f8def6
lumartin <yohagoelbien@gmail.com>
2020-06-12 23:01:44 +0200

    example: get and stringify

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,15 +1,20 @@
-import setText, {appendText, showWaiting, hideWaiting} from './results.mjs';
+import setText, { appendText, showWaiting, hideWaiting } from './results.mjs';
 
-export function get(){
+export function get() {
+  axios.get('http://localhost:3000/orders/1')
+    .then(({ data }) => {
+      setText(JSON.stringify(data));
+    });
 }
 
-export function getCatch(){
+export function getCatch() {
 }
 
-export function chain(){
+export function chain() {
 }
 
-export function chainCatch(){
+export function chainCatch() {
 }
-export function final(){
+
+export function final() {
 }
